1cb4a986209f56b2dd16b780955d21d8dfa9eb2e
Lautaro <Lautaro.perez@hotmail.com.ar>
2025-08-07 13:33:17 -0300

    Initial commit

---
+++ springtasks/src/test/java/com/springtasks/springtasks/SpringtasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtasks.springtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
